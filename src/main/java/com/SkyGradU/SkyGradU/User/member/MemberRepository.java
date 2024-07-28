e1f88aceb026a6abf86117db72f1570aa844427e
jjonghwa99 <jjonghwa2610@naver.com>
2024-07-29 00:43:56 +0900

    탈퇴기능 구현

--- src/main/java/com/SkyGradU/SkyGradU/User/member/MemberRepository.java
+++ src/main/java/com/SkyGradU/SkyGradU/User/member/MemberRepository.java
@@ -10,4 +10,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
    /* Optional<Member> findByUsername(String username);
     Optional<Member> findByUserEmail(String userEmail);
     Optional<Member> findBymajor(String usermajor);*/
+
+    void deleteByStudentID(String studentID);
 }
--- src/main/java/com/SkyGradU/SkyGradU/User/member/UpdateUserController.java
+++ src/main/java/com/SkyGradU/SkyGradU/User/member/UpdateUserController.java
@@ -1,18 +1,28 @@
 package com.SkyGradU.SkyGradU.User.member;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class UpdatePWController {
+public class UpdateUserController {
+
+    private final UserService userService;
 
     @Autowired
-    private UserService userService;
+    public UpdateUserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/checkPW")
     public Map<String, Object> verifyCurrentPassword(@RequestBody Map<String, String> request) {
@@ -45,5 +55,18 @@ public class UpdatePWController {
         }
 
         return response;
+    }
+
+    @DeleteMapping("/member/delete")
+    public ResponseEntity<String> deleteAccount(@AuthenticationPrincipal UserDetails userDetails,
+                                                HttpServletRequest request, HttpServletResponse response) {
+        String studentID = userDetails.getUsername();
+        userService.deleteMemberByStudentID(studentID);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return ResponseEntity.ok("회원탈퇴가 완료되었습니다.");
     }
 }
