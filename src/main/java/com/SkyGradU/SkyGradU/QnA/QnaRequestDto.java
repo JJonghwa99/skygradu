a6b82782e9c25aa5f70d7358614c0354600047b0
JJonghwa99 <jjonghwa2610@naver.com>
2025-04-02 00:48:43 +0900

    스타일 수정 및 게시물 저장 로직 수정

---
+++ src/main/java/com/SkyGradU/SkyGradU/QnA/QnaRequestDto.java
@@ -0,0 +1,12 @@
+package com.SkyGradU.SkyGradU.QnA;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QnaRequestDto {
+    private String title;
+    private String content;
+    private Boolean secret;
+}
