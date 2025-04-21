4a1a6302e82a733416963bc56eff2be13888a96a
KimJuHyeon <hjs851j@naver.com>
2025-04-21 14:39:57 +0900

    Update AllLecturesDTO.java

--- src/main/java/com/SkyGradU/SkyGradU/Lectures/AllLecturesDTO.java
+++ src/main/java/com/SkyGradU/SkyGradU/Lectures/AllLecturesDTO.java
@@ -12,11 +12,14 @@ public class AllLecturesDTO {
     private int credits;
     private String semesterCompleted;
 
+    private boolean completed;
+
     public AllLecturesDTO(AllLectures entity) {
-        this.lectureCode = entity.getLectureCode();
-        this.courseName = entity.getCourseName();
-        this.courseType = entity.getCourseType();
-        this.credits = entity.getCredits();
+        this.lectureCode       = entity.getLectureCode();
+        this.courseName        = entity.getCourseName();
+        this.courseType        = entity.getCourseType();
+        this.credits           = entity.getCredits();
         this.semesterCompleted = entity.getSemesterCompleted();
+        // completed는 Service 레이어에서 setCompleted(...)로 채워줍니다.
     }
 }
